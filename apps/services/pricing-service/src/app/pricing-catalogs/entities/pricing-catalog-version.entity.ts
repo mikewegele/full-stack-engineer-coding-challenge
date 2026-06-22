8875c217c29c133545cf9aba98ee5ce261f6a541
Mike Wegele <mikew01@mi.fu-berlin.de>
2026-06-22 10:54:12 +0200

    Reformat pricing and trade files

--- apps/services/pricing-service/src/app/pricing-catalogs/entities/pricing-catalog-version.entity.ts
+++ apps/services/pricing-service/src/app/pricing-catalogs/entities/pricing-catalog-version.entity.ts
@@ -1,4 +1,11 @@
-import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn, } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 import { PricingCatalogStatus } from './pricing-catalog.enums';
 import { PricingCatalogPosition } from './pricing-catalog-position.entity';
